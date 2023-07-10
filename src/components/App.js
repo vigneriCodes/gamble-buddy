78ec091e55c9b5382d7a940316ec13cb34add053
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-10 11:37:05 -0700

    add initial gamble(places)/components/stateSlice

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import Header from './Header';
+import GamblesList from './GamblesList';
+
+const initialGambles = [
+	{
+		id: 118836,
+		name: 'Wild Goose',
+		image: 'https://i.pravatar.cc/48?u=118836',
+		spent: 120,
+		winnings: 0,
+	},
+	{
+		id: 933372,
+		name: "Shari's",
+		image: 'https://i.pravatar.cc/48?u=933372',
+		spent: 60,
+		winnings: 475,
+	},
+	{
+		id: 499476,
+		name: 'Oak Tree',
+		image: 'https://i.pravatar.cc/48?u=499476',
+		spent: 60,
+		winnings: 0,
+	},
+	{
+		id: 853489,
+		name: 'Seven Feathers',
+		image: 'https://i.pravatar.cc/48?u=853489',
+		spent: 200,
+		winnings: 900,
+	},
+];
 
 function App() {
+	const [gambleDb, setGambleDb] = useState([]);
+
+	useEffect(() => {
+		setGambleDb([...initialGambles]);
+	}, []);
+
 	return (
 		<div className='App'>
-			<h1>GAMBLE BUDDY</h1>
+			<Header />
+			<GamblesList gambleDb={gambleDb} />
 		</div>
 	);
 }
